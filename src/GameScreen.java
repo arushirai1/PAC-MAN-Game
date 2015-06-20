33c10dd37eeee5a7a867d192165a361bfc333793
arushirai1 <arushirai1999@gmail.com>
2015-06-20 13:58:04 -0700

    Added images to program

--- src/GameScreen.java
+++ src/GameScreen.java
@@ -22,9 +22,10 @@ public class GameScreen extends JPanel implements ActionListener {
 	
 	public GameScreen() {
 		setBackground(Color.black);
+		setSize(500, 500);
 		pac =(new ImageIcon("pac.png")).getImage();
-		x_position = (int)Math.random()*500;
-		y_position = (int)Math.random()*500;
+		x_position = 250;
+		y_position = 250;
 	}
 	
 	public void move() {
@@ -48,10 +49,20 @@ public class GameScreen extends JPanel implements ActionListener {
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		g.drawImage(pac, x_position, y_position, this);
-		g.drawOval(x_position, y_position, 30, 30);
+		g.setColor(Color.YELLOW);
+		g.fillOval(x_position + 80, y_position+15, 20, 20);
 
 	}
-
+	
+	public void checkCollisions() {
+		
+	}
+	
+	//move, check for collisions, make a wall, create dots for the pac to eat, if all dots are eaten next game, create ghosts,
+	public void gameSetUp() {
+		//make a wall, create dots for the pac to eat,
+		
+	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
